8b0ec67a9708560583c32a47f8e008eac89ea0f6
Craig J Perry <craigp84@gmail.com>
2014-02-11 22:05:02 +0000

    Added initial package structure

---
+++ src/canbus_explorer/runner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""Application launcher."""
+
+import sys
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+if __name__ == "__main__":
+    main()
+
