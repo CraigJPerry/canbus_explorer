e429b2a6bebe3b4041f9684dca7eaa4977b87cd1
Craig J Perry <craigp84@gmail.com>
2014-03-19 22:48:12 +0000

    Satisfied unit test for program termination behaviour. Added module doc string at package root level.

--- src/canbus_explorer/runner.py
+++ src/canbus_explorer/runner.py
@@ -5,11 +5,32 @@
 """Application launcher."""
 
 import sys
+import argparse
+import canbus_explorer
 
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
+class ArgumentParsingError(Exception):
+    pass
+
+
+class ExceptionRaisingArgumentParser(argparse.ArgumentParser):
+
+    def error(self, message):
+        raise ArgumentParsingError(message)
+
+
+def parse_arguments():
+    parser = ExceptionRaisingArgumentParser(
+        description=canbus_explorer.__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
 
 if __name__ == "__main__":
     main()
--- src/tests/test_runner.py
+++ src/tests/test_runner.py
@@ -4,13 +4,14 @@
 
 import unittest
 from canbus_explorer import runner
+from canbus_explorer.runner import ArgumentParsingError
 
 
 class LaunchApplication(unittest.TestCase):
     """Ensure the app can be started."""
 
     def test_can_invoke_application(self):
-        self.assertRaises(SystemExit, runner.main)
+        self.assertRaises(ArgumentParsingError, runner.main)
 
 
 if __name__ == "__main__":
