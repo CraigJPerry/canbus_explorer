47c5af652a60c45594c14bacc75119c937dfaeb1
Craig J Perry <craigp84@gmail.com>
2014-03-19 23:38:24 +0000

    Exposed application version number internally

--- src/canbus_explorer/__init__.py
+++ src/canbus_explorer/__init__.py
@@ -7,3 +7,8 @@ engineering and debugging Canbus messages. I created this app to
 assist in reverse engineering the appropriate canbus messages to
 broadcast on my car’s canbus for automating various functions"""
 
+
+import pkg_resources
+
+__version__ = pkg_resources.require("canbus_explorer")[0].version
+
